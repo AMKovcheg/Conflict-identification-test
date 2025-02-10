389e4c8f507740c70a886c33d297190e9c516673
Nikolay Dundukov <dundukov.nv@phystech.edu>
2025-02-10 16:41:42 +0300

    back-up functions

--- functions.py
+++ functions.py
@@ -1,7 +1,15 @@
 def sum(l_int: list[int]) -> int:
     res = 0
-    pass
+
+    for i in l_int:
+        res += i
+
+    return res
 
 def concat(l_str: list[str]) -> str:
     res = ""
-    pass
+
+    for s in l_str:
+        res += s
+
+    return res
