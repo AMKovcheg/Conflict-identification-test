3bef9af81372ee39a44a18c3d5931b23f15b2bc8
Nikolay Dundukov <dundukov.nv@phystech.edu>
2025-02-10 17:33:22 +0300

    functions fixed

--- functions.py
+++ functions.py
@@ -1,5 +1,21 @@
-def sum(a: int, b: int) -> int:
+def sum2(a: int, b: int) -> int:
     return a + b
 
-def concat(s1: str, s2: str) -> str:
+def concat2(s1: str, s2: str) -> str:
     return s1 + s2
+
+def sum(l_int: list[int]) -> int:
+    res = 0
+
+    for i in l_int:
+        res += i
+
+    return res
+
+def concat(l_str: list[str]) -> str:
+    res = ""
+
+    for s in l_str:
+        res += s
+
+    return res
